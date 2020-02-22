f2ac281bac7e04bfb6163714a176b9096da9b74a
natali krasnov <nata555560@gmail.com>
2020-02-22 18:54:08 +0200

    first commit

---
+++ src/utils/messages.js
@@ -0,0 +1,20 @@
+const generateMessage = (username = 'Admin', text) => {
+    return {
+        username,
+        text,
+        createdAt: new Date().getTime()
+    }
+}
+
+const generateLocationMessage = (username='Admin', url) => {
+    return {
+        username,
+        url,
+        createdAt: new Date().getTime()
+    }
+}
+
+module.exports = {
+    generateMessage,
+    generateLocationMessage
+}
